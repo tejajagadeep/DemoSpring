1802b9db2ed4b4269682bdd52a39c3c9d89733a3
tejajagadeep <62947554+tejajagadeep@users.noreply.github.com>
2023-11-08 15:39:15 +0530

    Update Hello.java

--- coding-files/Hello.java
+++ coding-files/Hello.java
@@ -1,45 +1,107 @@
-interface Drawable {
-    void draw();
+import java.util.ArrayList;
+import java.util.List;
+
+interface Product {
+    String getName();
+    double getPrice();
 }
 
-class Circle implements Drawable {
+class PhysicalProduct implements Product {
+    private String name;
+    private double price;
+
+    public PhysicalProduct(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
     @Override
-    public void draw() {
-        System.out.println("Drawing a circle");
+    public String getName() {
+        return name;
     }
-}
 
-class Rectangle implements Drawable {
     @Override
-    public void draw() {
-        System.out.println("Drawing a rectangle");
+    public double getPrice() {
+        return price;
     }
 }
 
-class Triangle implements Drawable {
+class DigitalProduct implements Product {
+    private String name;
+    private double price;
+
+    public DigitalProduct(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
     @Override
-    public void draw() {
-        System.out.println("Drawing a triangle");
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
     }
 }
 
-class CanvasRenderer {
-    void renderShape(Drawable shape) {
-        shape.draw();
+class Order {
+    private List<Product> products = new ArrayList<>();
+    private double totalAmount;
+
+    public void addProduct(Product product) {
+        products.add(product);
+        totalAmount += product.getPrice();
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void checkout(PaymentProcessor paymentProcessor) {
+        if (paymentProcessor.processPayment(totalAmount)) {
+            ShippingService shippingService = new ShippingService();
+            shippingService.shipProducts(products);
+            System.out.println("Order processed successfully.");
+        } else {
+            System.out.println("Payment failed. Order not processed.");
+        }
     }
 }
 
-public class ShapeDrawingApp {
+interface PaymentProcessor {
+    boolean processPayment(double amount);
+}
+
+class MyPaymentProcessor implements PaymentProcessor {
+    @Override
+    public boolean processPayment(double amount) {
+        // Implement PayPal payment processing logic
+        System.out.println("Processing payment via PayPal...");
+        return true; // Payment is successful in this simplified example
+    }
+}
+
+class ShippingService {
+    public void shipProducts(List<Product> products) {
+        for (Product product : products) {
+            // Implement shipping logic for each product
+            System.out.println("Shipping: " + product.getName());
+        }
+    }
+}
+
+public class ECommerceApp {
     public static void main(String[] args) {
-        CanvasRenderer canvas = new CanvasRenderer();
+        Product product1 = new PhysicalProduct("Laptop", 1000.0);
+        Product product2 = new DigitalProduct("Software Download", 50.0);
+        Order order = new Order();
 
-        Drawable circle = new Circle();
-        Drawable rectangle = new Rectangle();
-        Drawable triangle = new Triangle();
+        order.addProduct(product1);
+        order.addProduct(product2);
 
-        // Render different shapes on the canvas
-        canvas.renderShape(circle);
-        canvas.renderShape(rectangle);
-        canvas.renderShape(triangle);
+        PaymentProcessor paymentProcessor = new MyPaymentProcessor();
+        order.checkout(paymentProcessor);
     }
 }
