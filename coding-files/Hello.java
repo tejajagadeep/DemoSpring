0886d8e8f638e1717e1f07b6020ec530d46b1b63
tejajagadeep <62947554+tejajagadeep@users.noreply.github.com>
2023-11-14 17:20:45 +0530

    Update Hello.java

--- coding-files/Hello.java
+++ coding-files/Hello.java
@@ -1,107 +1,93 @@
-import java.util.ArrayList;
+package com.stackroute.service;
+
+import com.stackroute.exception.BlogAlreadyExistsException;
+import com.stackroute.exception.BlogNotFoundException;
+import com.stackroute.repository.BlogRepository;
+import com.stackroute.domain.Blog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.Optional;
 
-interface Product {
-    String getName();
-    double getPrice();
-}
+/**
+ * @Service indicates annotated class is a service which hold business logic in the Service layer
+ */
+@Service
+public class BlogServiceImpl implements BlogService {
+    private BlogRepository blogRepository;
 
-class PhysicalProduct implements Product {
-    private String name;
-    private double price;
-
-    public PhysicalProduct(String name, double price) {
-        this.name = name;
-        this.price = price;
+    /**
+     * Constructor based Dependency injection to inject BlogRepository here
+     */
+    @Autowired
+    public BlogServiceImpl(BlogRepository blogRepository) {
+        this.blogRepository = blogRepository;
     }
 
+    /**
+     * save a new blog
+     */
     @Override
-    public String getName() {
-        return name;
+    public Blog saveBlog(Blog blog) {
+        // Check if a blog with the same ID already exists
+        if (blogRepository.existsById(blog.getBlogId())) {
+            throw new BlogAlreadyExistsException("Blog with ID " + blog.getBlogId() + " already exists.");
+        }
+        return blogRepository.save(blog);
     }
 
+    /**
+     * retrieve all blogs
+     */
     @Override
-    public double getPrice() {
-        return price;
-    }
-}
-
-class DigitalProduct implements Product {
-    private String name;
-    private double price;
-
-    public DigitalProduct(String name, double price) {
-        this.name = name;
-        this.price = price;
+    public List<Blog> getAllBlogs() {
+        return (List<Blog>) blogRepository.findAll();
     }
 
+
+    /**
+     * retrieve blog by id
+     */
     @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public double getPrice() {
-        return price;
-    }
-}
-
-class Order {
-    private List<Product> products = new ArrayList<>();
-    private double totalAmount;
-
-    public void addProduct(Product product) {
-        products.add(product);
-        totalAmount += product.getPrice();
-    }
-
-    public double getTotalAmount() {
-        return totalAmount;
-    }
-
-    public void checkout(PaymentProcessor paymentProcessor) {
-        if (paymentProcessor.processPayment(totalAmount)) {
-            ShippingService shippingService = new ShippingService();
-            shippingService.shipProducts(products);
-            System.out.println("Order processed successfully.");
+    public Blog getBlogById(int id) {
+        Optional<Blog> optional = blogRepository.findById(id);
+        if (optional.isPresent()) {
+            return optional.get();
         } else {
-            System.out.println("Payment failed. Order not processed.");
+            throw new BlogNotFoundException("Blog not found with ID " + id);
         }
     }
-}
 
-interface PaymentProcessor {
-    boolean processPayment(double amount);
-}
-
-class MyPaymentProcessor implements PaymentProcessor {
+    /**
+     * delete blog by id
+     */
     @Override
-    public boolean processPayment(double amount) {
-        // Implement PayPal payment processing logic
-        System.out.println("Processing payment via PayPal...");
-        return true; // Payment is successful in this simplified example
+    public Blog deleteBlog(int id) {
+        Optional<Blog> optional = blogRepository.findById(id);
+        if (optional.isPresent()) {
+            Blog blog = optional.get();
+            blogRepository.deleteById(id);
+            return blog;
+        } else {
+            throw new BlogNotFoundException("Blog not found with ID " + id);
+        }
     }
-}
 
-class ShippingService {
-    public void shipProducts(List<Product> products) {
-        for (Product product : products) {
-            // Implement shipping logic for each product
-            System.out.println("Shipping: " + product.getName());
+    /**
+     * update blog
+     */
+    @Override
+    public Blog updateBlog(Blog blog) {
+        Optional<Blog> optional = blogRepository.findById(blog.getBlogId());
+        if (optional.isPresent()) {
+            Blog existingBlog = optional.get();
+            existingBlog.setBlogContent(blog.getBlogContent());
+            return saveBlog(existingBlog);
+        } else {
+            throw new BlogNotFoundException("Blog not found with ID " + blog.getBlogId());
         }
     }
 }
 
-public class ECommerceApp {
-    public static void main(String[] args) {
-        Product product1 = new PhysicalProduct("Laptop", 1000.0);
-        Product product2 = new DigitalProduct("Software Download", 50.0);
-        Order order = new Order();
-
-        order.addProduct(product1);
-        order.addProduct(product2);
-
-        PaymentProcessor paymentProcessor = new MyPaymentProcessor();
-        order.checkout(paymentProcessor);
-    }
-}
