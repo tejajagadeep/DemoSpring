ab648082b12d45d29001301f9401d6e95dc4a549
tejajagadeep <62947554+tejajagadeep@users.noreply.github.com>
2023-11-23 08:42:20 +0530

    Update Hello.java

--- coding-files/Hello.java
+++ coding-files/Hello.java
@@ -1,159 +1,31 @@
-package com.stackroute.test.service;
+public class AuthService {
+    @Autowired
+    AuthenticationClient authenticationClient;
 
-import com.stackroute.domain.Blog;
-import com.stackroute.exception.BlogAlreadyExistsException;
-import com.stackroute.exception.BlogNotFoundException;
-import com.stackroute.repository.BlogRepository;
-import com.stackroute.service.BlogServiceImpl;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
+    public Map<String, String> validateToken(String token) {
+        HttpEntity<String> entity = getHttpEntityWithHeaders(token);
+        try {
+            ResponseEntity<Map<String, String>> response = authenticationClient.validateToken(token);
+//                    restTemplate.exchange
+//                            (baseUrl + "/validate", HttpMethod.POST,
+//                                    entity, new ParameterizedTypeReference<Map<String, String>>() {
+//            });
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-
-@ExtendWith(MockitoExtension.class)
-class BlogServiceTest {
-    // Mock BlogRepository
-    @Mock
-    private BlogRepository blogRepository;
-    // Inject Mocks to BlogServiceImpl
-    @InjectMocks
-    private BlogServiceImpl blogServiceImpl;
-    @Mock
-    private Blog blog, blog1;
-    private List<Blog> blogList;
-    private Optional optional;
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        blog = new Blog(1, "SampleBlog", "Imneet", "SampleBlogforTesting");
-        blog1 = new Blog(2, "Blog 1", "John", "Sample Blog 1 for Testing");
-        optional = Optional.of(blog);
+            System.out.println(response.getBody()+"from*********");
+            System.out.println(response.getBody()+"from*********");
+            if (response.getStatusCode() == HttpStatus.OK) {
+                return response.getBody();
+            } else {
+                throw new RuntimeException("Failed to validate token: " + response.getStatusCode());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("connection refused: " + e.getMessage());
+        }
     }
 
-    @AfterEach
-    public void tearDown() {
-        blog = null;
-        blogRepository.deleteAll();
-    }
-
-    /*
-     * write test case for saveBlog() method, by mocking the repository
-     * */
-    @Test
-    public void givenBlogToSaveThenShouldReturnSavedBlog() {
-        when(blogRepository.save(blog)).thenReturn(blog);
-                assertThat(blogServiceImpl.saveBlog(blog)).isEqualTo(blog);
-    }
-
-    /*
-     * write test case for asserting RuntimeException when saveBlog() method is called, by mocking the repository
-     */
-    @Test
-    public void givenBlogToSaveThenShouldNotReturnSavedBlog() {
-        Blog blog3 = new Blog(1, "SampleBlog", "Imneet", "SampleBlogforTesting");
-        when(blogRepository.existsById(blog.getBlogId())).thenReturn(true);
-        BlogAlreadyExistsException exception = assertThrows(
-                BlogAlreadyExistsException.class,
-                ()->blogServiceImpl.saveBlog(blog3)
-        );
-        String expected = "Blog with ID " + blog.getBlogId() + " already exists.";
-        String actual = exception.getMessage();
-        assert(expected.contains(actual));
-    }
-
-    /*
-     * write test case for getAllBlogs() method, by mocking the repository
-     */
-    @Test
-    public void givenGetAllBlogsThenShouldReturnListOfAllBlogs() {
-        when(blogRepository.findAll()).thenReturn(new ArrayList<>(Collections.singleton(blog)));
-        assertEquals(1, blogServiceImpl.getAllBlogs().size());
-//    	assertThat(blogServiceImpl.getAllBlogs().get(0).getAuthorName()).isEqualTo(blog.getAuthorName());
-    }
-
-    /*
-     * write test case for getBlogById() method, by mocking the repository
-     */
-    @Test
-    public void givenBlogIdThenShouldReturnRespectiveBlog() {
-        when(blogRepository.findById(blog.getBlogId())).thenReturn(optional);
-        assertEquals(blog, blogServiceImpl.getBlogById(blog.getBlogId()));
-    }
-
-    /*
-     * write test case for deleteBlog() method, by mocking the repository
-     */
-    @Test
-    void givenBlogIdToDeleteThenShouldReturnDeletedBlog() {
-
-        when(blogRepository.findById(1)).thenReturn(optional);
-        blogServiceImpl.deleteBlog(blog.getBlogId());
-
-        // Then
-        verify(blogRepository, times(1)).deleteById(blog.getBlogId());
-    }
-
-    /*
-     * write test case for asserting BlogNotFoundException when deleteBlog() method is called, by mocking the repository
-     */
-    @Test
-    void givenBlogIdToDeleteThenShouldNotReturnDeletedBlog() {
-        when(blogRepository.findById(1)).thenReturn(Optional.empty());
-        assertThrows(BlogNotFoundException.class, ()->blogServiceImpl.deleteBlog(blog.getBlogId()));
-    }
-
-    /*
-     * write test case for updateBlog() method, by mocking the repository
-     */
-    @Test
-    public void givenBlogToUpdateThenShouldReturnUpdatedBlog() {
-        Blog updatedBlog = new Blog(1, "SampleBlog", "Imneet", "SampleBlogforTestingUpdation");
-        when(blogRepository.findById(1)).thenReturn(optional);
-        when(blogRepository.save(any(Blog.class))).thenReturn(updatedBlog);
-        assertEquals(updatedBlog, blogServiceImpl.updateBlog(blog));
-    }
-
-    /*
-     * write test case for asserting BlogNotFoundException when updateBlog() method is called, by mocking the repository
-     */
-    @Test
-    public void givenBlogToUpdateThenShouldNotReturnUpdatedBlog() {
-
-        when(blogRepository.findById(1)).thenReturn(Optional.empty());
-        assertThrows(BlogNotFoundException.class, ()->blogServiceImpl.updateBlog(blog));
-    }
-
-
-    /*
-     * write test case for asserting BlogNotFoundException when getBlogById() method is called, by mocking the repository
-     */
-    @Test
-    public void givenNonExistentBlogIdThenShouldThrowBlogNotFoundException() {
-        when(blogRepository.findById(1)).thenReturn(Optional.empty());
-        assertThrows(BlogNotFoundException.class, ()->blogServiceImpl.getBlogById(blog.getBlogId()));
-    }
-
-    /*
-     * write test case for asserting BlogNotFoundException when updateBlog() method is called, by mocking the repository
-     */
-    @Test
-    public void givenUpdateNonExistentBlogThenShouldThrowBlogNotFoundException() {
-        when(blogRepository.findById(1)).thenReturn(Optional.empty());
-        assertThrows(BlogNotFoundException.class, ()->blogServiceImpl.updateBlog(blog));
+    private HttpEntity<String> getHttpEntityWithHeaders(String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", token);
+        return new HttpEntity<>("", headers);
     }
 }
